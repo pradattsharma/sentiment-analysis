2b3524fdd638443d223b33a0502a263f04283b17
pradattsharma <sharmapradatt@gmail.com>
2020-08-06 19:03:07 +0530

    Delete twitterkeys.py

--- twitterkeys.py
+++
@@ -1,4 +0,0 @@
-ckey = 'M5K6tGzeHoibdtD76TknjgmLh'
-csecret = 'qcaFoPXb1c2GiaAQuVUwnAdTob5YssyQyb59P79fu5ReximqqP'
-atoken = '1221108262600237056-nFe534c5khHM4ZVNdt4piNofqA7gPB'
-asecret = 'Pu0upbG92qz38y0oqOK3pMCgIUN9AtvXnE9vJJafW1Ad5'
